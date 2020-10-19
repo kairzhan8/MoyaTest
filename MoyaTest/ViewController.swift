1f98ca22d968885604bf4b14089a1ce38752236c
kairzhan8 <kairzhankural@gmail.com>
2020-10-19 19:18:25 +0600

    Initial Commit

---
+++ MoyaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoyaTest
+//
+//  Created by Kairzhan Kural on 10/19/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
